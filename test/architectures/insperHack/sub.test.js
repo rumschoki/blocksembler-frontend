92b25fe0a119b8d5378e4c821ec4242175791123
rumschoki <amy3st@gmail.com>
2025-06-01 14:59:32 +0200

    basic executeOn works

--- test/architectures/insperHack/sub.test.js
+++ test/architectures/insperHack/sub.test.js
@@ -1,46 +1,46 @@
-import { SubwInstruction, InsperHackInstructionFactory } from "@/architectures/insperHack/instructions";
+import { SubInstruction, InsperHackInstructionFactory } from "@/architectures/insperHack/instructions";
 import {expect, test, vi} from "vitest";
 import {Word} from "../../../src/architectures/system";
 
-//subw (%A), %D, %D
-test("Test 1.1: subw instruction to machine code (a=1)", () => {
-    let instruction = new SubwInstruction(['(%A)', '%D', '%D']);
+//sub-(%A), %D, %D
+test("Test 1.1: sub-instruction to machine code (a=1)", () => {
+    let instruction = new SubInstruction(['(%A)', '%D', '%D']);
     // 111 a cccccc ddd jjj
     let expectedCode = "111" + "1" + "000111" + "010" + "000";
 
     expect(instruction.toMachineCode()).toBe(expectedCode);
 });
 
-test("Test 1.2: subw instruction to machine code (a=0)", () => {
-    let instruction = new SubwInstruction(['%A', '%D', '%D']);
+test("Test 1.2: sub-instruction to machine code (a=0)", () => {
+    let instruction = new SubInstruction(['%A', '%D', '%D']);
     // 111 a cccccc ddd jjj
     let expectedCode = "111" + "0" + "000111" + "010" + "000";
 
     expect(instruction.toMachineCode()).toBe(expectedCode);
 });
 
-test("Test 2.1: creating subw instruction from mnemonic (memory access)", () => {
+test("Test 2.1: creating sub-instruction from mnemonic (memory access)", () => {
     let factory = new InsperHackInstructionFactory();
 
-    let inst = factory.createFromMnemonic("subw", ['(%A)', '%D', '%D']);
-    let expected = new SubwInstruction(['(%A)', '%D', '%D']);
+    let inst = factory.createFromMnemonic("Sub", ['(%A)', '%D', '%D']);
+    let expected = new SubInstruction(['(%A)', '%D', '%D']);
 
     expect(inst).toMatchObject(expected);
 });
 
-test("Test 2.1: creating subw instruction from mnemonic", () => {
+test("Test 2.1: creating sub-instruction from mnemonic", () => {
     let factory = new InsperHackInstructionFactory();
 
-    let inst = factory.createFromMnemonic("subw", ['%A', '%D', '%D']);
-    let expected = new SubwInstruction(['%A', '%D', '%D']);
+    let inst = factory.createFromMnemonic("Sub", ['%A', '%D', '%D']);
+    let expected = new SubInstruction(['%A', '%D', '%D']);
 
     expect(inst).toMatchObject(expected);
 });
 
-test("Test 3: create subw instruction from machine code", () => {
+test("Test 3: create sub-instruction from machine code", () => {
     let machineCode = "111" + "1" + "000111" + "010" + "000";
 
-    let expectedInstruction = new SubwInstruction(['(%A)', '%D', '%D']);
+    let expectedInstruction = new SubInstruction(['(%A)', '%D', '%D']);
 
     let factory = new InsperHackInstructionFactory();
     let instruction = factory.createFromOpCode([Word.fromString(machineCode)], 0);
@@ -61,7 +61,7 @@ test("Test 4.1: substract two registers and store to different register", () => 
         },
     };
 
-    let instruction = new SubwInstruction(['%A', '%D', '%D']);
+    let instruction = new SubInstruction(['%A', '%D', '%D']);
 
     instruction.executeOn(emulator);
 
@@ -83,7 +83,7 @@ test("Test 4.2: substract two registers and store to different register", () => 
         memory: [Word.fromSignedIntValue(10)],
     };
 
-    let instruction = new SubwInstruction(['(%A)', '%D', '%D']);
+    let instruction = new SubInstruction(['(%A)', '%D', '%D']);
 
     instruction.executeOn(emulator);
 
